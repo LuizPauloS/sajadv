bcb4742206e8e490d5733b592f4ea20f6c7446a0
luiz <luiz.paulo.09@hotmail.com>
2020-06-12 14:50:06 -0400

    #Commitando refatoração de nome de classe principal

--- sajadv-persons-api/src/main/java/br/com/portfolio/lsilva/sajadv/SajadvPersonsApplication.java
+++ sajadv-persons-api/src/main/java/br/com/portfolio/lsilva/sajadv/SajadvPersonsApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class SajadvApplication {
+public class SajadvPersonsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SajadvApplication.class, args);
+		SpringApplication.run(SajadvPersonsApplication.class, args);
 	}
 
 }
