7797a6f68c3ff02ecc4b4f9305905f8c234f8c59
luiz <luiz.paulo.09@hotmail.com>
2020-06-28 15:27:09 -0400

    #Commitando refatoração de ibase service e adicionando refresh scope para atualizar configurações

--- sajadv-core/src/main/java/br/com/portfolio/lsilva/sajadv/service/IBaseService.java
+++ sajadv-core/src/main/java/br/com/portfolio/lsilva/sajadv/service/IBaseService.java
@@ -3,8 +3,6 @@ package br.com.portfolio.lsilva.sajadv.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface IBaseService<T> {
 
     T save(T t);
--- sajadv-persons-api/src/main/java/br/com/portfolio/lsilva/sajadv/SajadvPersonsApplication.java
+++ sajadv-persons-api/src/main/java/br/com/portfolio/lsilva/sajadv/SajadvPersonsApplication.java
@@ -3,8 +3,10 @@ package br.com.portfolio.lsilva.sajadv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@RefreshScope
 @EnableCaching
 @EnableEurekaClient
 @SpringBootApplication
